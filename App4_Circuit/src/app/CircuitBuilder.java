205cd1a4d1554225608eaa6a2d86866f5ffe1b53
antoineTremblay1007 <2540948@cegeplimoilou.ca>
2026-03-31 12:16:49 -0400

    methode lireComposant

--- App4_Circuit/src/app/CircuitBuilder.java
+++ App4_Circuit/src/app/CircuitBuilder.java
@@ -1,6 +1,13 @@
 package app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import electronique.CircuitParallele;
+import electronique.CircuitSerie;
 import electronique.Composant;
+import electronique.Resistance;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CircuitBuilder {
 
@@ -10,7 +17,25 @@ public class CircuitBuilder {
         return null;
     }
 
-    private Composant lireComposant(){
-        return null;
+    private Composant lireComposant(JsonNode node){
+        String type = node.get("type").asText();
+
+        if ("resistance".equals(type)) {
+            return new Resistance(node.get("valeur").asDouble());
+        }else if ("serie".equals(type)) {
+            List<Composant> composant = new ArrayList<>();
+            for(JsonNode composantNode : node.get("composants")) {
+                composant.add(lireComposant(composantNode));
+            }
+            return new CircuitSerie(composant);
+        }
+        else if("parallele".equals(type)) {
+            List<Composant> composants = new ArrayList<>();
+            for(JsonNode composantsNode : node.get("composants")) {
+                composants.add(lireComposant(composantsNode));
+            }
+            return new CircuitParallele(composants);
+        }
+        throw new IllegalArgumentException("Type de circuit invalide : " + type);
     }
 }
